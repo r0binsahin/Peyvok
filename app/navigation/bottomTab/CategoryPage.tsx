17fd316142be159466701a29cc44b8f8b98e5019
r0binSahin <robin.sahin88@gmail.com>
2023-12-13 20:05:56 +0100

    (feat): nav file added, not working

---
+++ app/navigation/bottomTab/CategoryPage.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from 'react-native';
+
+export const CategoryPage = () => {
+  return (
+    <View>
+      <Text>Category</Text>
+    </View>
+  );
+};
--- app/services/seedData.ts
+++ app/services/seedData.ts
@@ -12,7 +12,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1F5K88Mhmq5x8uXw-ofWXVBXiMl-PPYrh/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1F5K88Mhmq5x8uXw-ofWXVBXiMl-PPYrh',
       category: 'colors',
     }) as Word;
     realm.create('Word', {
@@ -21,7 +21,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1r1h4_jb4xgx2GChfvNuCbPlKY5TYeB8D/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1r1h4_jb4xgx2GChfvNuCbPlKY5TYeB8D',
       category: 'colors',
     }) as Word;
     realm.create('Word', {
@@ -30,7 +30,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1yx2L-5cyy07_H_JFagcboZXeDx2KAloq/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1yx2L-5cyy07_H_JFagcboZXeDx2KAloq',
       category: 'colors',
     }) as Word;
 
@@ -40,7 +40,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1ehSIt-2Hgn5-SVusMmPglqaexywZC7RN/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1ehSIt-2Hgn5-SVusMmPglqaexywZC7RN',
       category: 'colors',
     }) as Word;
 
@@ -50,7 +50,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1-Eq2KG0FuPeoNjAvwZGDjm1u8VdIoWU2/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1-Eq2KG0FuPeoNjAvwZGDjm1u8VdIoWU2',
       category: 'animals',
     }) as Word;
 
@@ -60,7 +60,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1vOk1oYlhsbqOH9ojwN8GIeT6wHkqwkTs/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1vOk1oYlhsbqOH9ojwN8GIeT6wHkqwkTs',
       category: 'animals',
     }) as Word;
 
@@ -70,7 +70,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1GpT5mS_Fd9SJzjtH1xGB-N0Q5GpWbf7G/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1GpT5mS_Fd9SJzjtH1xGB-N0Q5GpWbf7G',
       category: 'animals',
     }) as Word;
 
@@ -80,7 +80,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1GvKrlWzSwGAzbgXhjXotKFgxjrGCypOE/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1GvKrlWzSwGAzbgXhjXotKFgxjrGCypOE',
       category: 'animals',
     }) as Word;
 
@@ -90,7 +90,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1AoYH584E2-1jivqPIBopESIJZq_EzCU_/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1AoYH584E2-1jivqPIBopESIJZq_EzCU_',
       category: 'fruits',
     }) as Word;
 
@@ -100,7 +100,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1VwmoXPD0Mo3JoSpzc_3UY2C-1jcEIj-2/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1VwmoXPD0Mo3JoSpzc_3UY2C-1jcEIj-2',
       category: 'fruits',
     }) as Word;
 
@@ -110,7 +110,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1So_iT2LOzURJHAml8RCQEQucScjxghYd/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1So_iT2LOzURJHAml8RCQEQucScjxghYd',
       category: 'fruits',
     }) as Word;
 
@@ -120,7 +120,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1jIZeSmiKlKAGt_sYqNGLfTx_NUm1RhLJ/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1jIZeSmiKlKAGt_sYqNGLfTx_NUm1RhLJ',
       category: 'fruits',
     }) as Word;
 
@@ -130,7 +130,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1nk0DMtA7t1YEOxq0odwRMJP-I7aNeK9h/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1nk0DMtA7t1YEOxq0odwRMJP-I7aNeK9h',
       category: 'geometric forms',
     }) as Word;
 
@@ -140,7 +140,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1zyCZXXptuCuYVNP0_Rr_wYnWh2fP5Xsl/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1zyCZXXptuCuYVNP0_Rr_wYnWh2fP5Xsl',
       category: 'geometric forms',
     }) as Word;
 
@@ -150,7 +150,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1uwMdUYHee9PR9VjQYpRlhY6aSslRJMBG/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1uwMdUYHee9PR9VjQYpRlhY6aSslRJMBG',
       category: 'geometric forms',
     }) as Word;
 
@@ -160,7 +160,7 @@ export const data = useCallback(() => {
       image:
         'https://i.postimg.cc/Y9d1f9qz/360-F-470299797-UD0eo-VMMSUb-HCc-NJCdv2t8-B2g1-GVq-Ygs.jpg',
       audio:
-        'https://drive.google.com/file/d/1YlhNaIfxq7A9G7FJWY2xdkmDjTA___BS/view?usp=drive_link',
+        'https://drive.google.com/uc?export=media&id=1YlhNaIfxq7A9G7FJWY2xdkmDjTA___BS',
       category: 'geometric forms',
     }) as Word;
   });
