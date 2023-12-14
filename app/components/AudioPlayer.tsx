f322a590753e8b984f51c1fdf4a9ffc7544795b4
r0binSahin <robin.sahin88@gmail.com>
2023-12-14 11:34:25 +0100

    (feat): category comp

--- app/components/AudioPlayer.tsx
+++ app/components/AudioPlayer.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 import {Word} from '../models/Word';
