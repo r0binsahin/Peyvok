25437864f60914925bf9deea0b50904303e3e100
r0binSahin <robin.sahin88@gmail.com>
2023-12-28 10:46:39 +0100

    (refactor) app folder renamed to src

--- index.js
+++ index.js
@@ -5,9 +5,8 @@
 import {AppRegistry} from 'react-native';
 
 import {name as appName} from './app.json';
-import {AppWrapper} from './app/components/AppWrapper';
+import {AppWrapper} from './src/components/AppWrapper';
 import TrackPlayer from 'react-native-track-player';
-import Background from './app/components/backgroundComponents/Background';
 
-TrackPlayer.registerPlaybackService(() => require('./app/utiles/audioService'));
+TrackPlayer.registerPlaybackService(() => require('./src/utiles/audioService'));
 AppRegistry.registerComponent(appName, () => AppWrapper);
