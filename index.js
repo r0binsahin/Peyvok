edc20cb15acd5012afd86174cc8f22e663c8dd29
r0binSahin <robin.sahin88@gmail.com>
2024-01-03 11:34:06 +0100

    (feat): react native upgraded

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import {AppWrapper} from './src/components/AppWrapper';
 import TrackPlayer from 'react-native-track-player';
+import 'react-native-gesture-handler';
 
 TrackPlayer.registerPlaybackService(() => require('./src/utiles/audioService'));
 AppRegistry.registerComponent(appName, () => AppWrapper);
